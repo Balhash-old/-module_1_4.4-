7c3cb2eef335de3525a68a33933c0ac6efb3333c
Balhash-old <tel-alex@mail.ru>
2024-08-14 13:22:07 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_snring=input('Введите текст:  ')
+print(my_snring .upper())
+print(my_snring.lower())
+print(my_snring.replace(' '  ,'#'))
+print(my_snring .replace(' ',''))
+s=my_snring
+print( s[0])
+print(s[-1])
+print(len(my_snring))
